392dbd33d8e289b773669fc43b09d1c6e0438c54
sheikh ariful <sheikharifulislam770@gmail.com>
2022-04-16 22:16:46 +0600

    add user upload controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,45 +1,73 @@
-const databaseModel = require('../model/databaseModel');
-const {allUsersCollection} = databaseModel();
+const databaseModel = require("../model/databaseModel");
+const { allUsersCollection } = databaseModel();
 
-exports.allUsers = async(req, res, next) => {
-    try{
+exports.allUsers = async (req, res, next) => {
+    try {
         const users = await allUsersCollection.find({}).toArray();
         res.status(200).json(users);
-    }
-    catch(error){
+    } catch (error) {
         next(error);
     }
-}
+};
 
-exports.checkAdmin = async(req, res, next) => {
-    try{
-        const {userEmail} = req.query;        
-        const user = await allUsersCollection.findOne(
-            {
-                email: userEmail
-            }
-        );
+exports.checkAdmin = async (req, res, next) => {
+    try {
+        const { userEmail } = req.query;
+        const user = await allUsersCollection.findOne({
+            email: userEmail,
+        });
 
         let isAdmin = false;
 
-        if(user?.role === 'admin') {
+        if (user?.role === "admin") {
             isAdmin = true;
         }
 
-        res.status(200).json({isAdmin});
-    }
-    catch(error) {
+        res.status(200).json({ isAdmin });
+    } catch (error) {
         next(error);
     }
-}
+};
 
-exports.addUser = async(req, res, next) => {
-    try{
-        const user = req.body;            
+exports.addUser = async (req, res, next) => {
+    try {
+        const user = req.body;
         const result = await allUsersCollection.insertOne(user);
-        res.status(201).json(result);  
-    }
-    catch(error) {
+        res.status(201).json(result);
+    } catch (error) {
         next(error);
     }
-}
+};
+
+exports.singleUser = async (req, res) => {
+    try {
+        const { userEmail } = req.query;
+        const user = await allUsersCollection.findOne({ email: userEmail });
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+        const { userEmail } = req.query;
+        const result = await allUsersCollection.updateOne(
+            {
+                email: userEmail,
+            },
+            {
+                $set: {
+                    photoUrl: req.file.path,
+                    ...req.body,
+                },
+            }
+        );
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(200).json({
+            success: true,
+            error: err.message,
+        });
+    }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
-const UserRouter = require('express').Router();
+const UserRouter = require("express").Router();
+const upload = require("../middleware/fileUpload");
 const {
     allUsers,
     checkAdmin,
-    addUser
-} = require('../controller/userController');
+    addUser,
+    singleUser,
+    updateUser,
+} = require("../controller/userController");
 
-UserRouter.get('/all-user',allUsers);
-UserRouter.get('/check-admin',checkAdmin);
-UserRouter.post('/add-user',addUser);
+UserRouter.get("/all-user", allUsers);
+UserRouter.get("/check-admin", checkAdmin);
+UserRouter.post("/add-user", addUser);
+UserRouter.get("/single-user", singleUser);
+UserRouter.patch("/update-single-user", upload.single("profilePic"), updateUser);
 
 module.exports = UserRouter;
